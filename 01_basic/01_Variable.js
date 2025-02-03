fd77590057740e3c2964c871968461c53a80a7b3
suresh choudhary <118955464+sschoudhary30@users.noreply.github.com>
2025-02-03 12:31:12 +0000

    custome 3

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "sureshbhai@gmail.com"
+var accountpasskey = "12345"
+accountCity = "pali"
+
+// const ka matlab lock kar naa. const can't be changed
+
+accountId = 2
+
+console.log(accountId);
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 A code repo for javascript.
 
 To setup env
-View -> command platter
+View -> command paletter
+Then search container -> create config
+then node.js & javascipt.
+
+# To off Machine 

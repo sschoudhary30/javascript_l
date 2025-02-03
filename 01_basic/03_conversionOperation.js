ddf407b421597ac30b20bf7fde61eb0d18b33105
suresh choudhary <118955464+sschoudhary30@users.noreply.github.com>
2025-02-03 15:56:16 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = 33
+score= false // let declare follow.
+
+let valueInNumber = Number(score); // this number which string to number or fix hai
+
+console.log( typeof score); // way one
+console.log( typeof (score));
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber); // this will give number when string of number
+// if string "33" then output is 33
+// but if string "33abc" then output is NaN.
+// value is null then output is 0
+// true/false 1/0 respect
+// undefine = undefine
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "suresh"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn) 
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let names = 33;
+
+let StringCon = String(names)
+
+console.log(typeof StringCon);
+console.log(StringCon);
+// this converstion are more usefull in javascript from.
+

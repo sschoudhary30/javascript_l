455ebebf266b01e1e0393cb973d6e4052b3e73d0
suresh choudhary <118955464+sschoudhary30@users.noreply.github.com>
2025-02-11 12:53:23 +0000

    day 8 js

---
+++ 08_events/Async.md
@@ -0,0 +1,20 @@
+## javascipt is synchronous.
+# single threaded.
+
+// this is by defualt;
+
+excution context;
+excute one line of code at a time.
+
+blocking code -> block the flow of program
+read file sync
+
+non blocking code
+does not block excution 
+read file async
+
+to make async and sync depend or requirement.
+
+fetch() new api and add new high priorty queue.
+
+
---
+++ 09_advance/promises.js
@@ -0,0 +1,100 @@
+// 
+// promises making
+// promise is object.
+const promiseOne = new Promise((resolve , reject)=>
+    {
+        // Do an async task
+        // DB calls, cryptography, network
+
+        setTimeout(function(){
+            console.log('Asyn task is compelete');
+            resolve();
+        },1000)
+});
+
+// phele promise part nahi tha core js then ye sab blue lib k help se promise apply hotha tha
+
+// .then is directly related to resolve
+promiseOne.then(function(){
+    //console.log("promise consumed");
+
+})
+
+new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        //console.log("Async 2 hai bhai");
+        resolve()
+    },1000);
+}).then(()=>{
+    //console.log("async 2 resolve");
+})
+
+
+const PromiseThree = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve({username:"chai", email:"bhai@suresh.com"})
+    },1000)
+
+})
+
+PromiseThree.then((user)=>{
+    //console.log(user);
+})
+
+const promiseFour = new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({username:'suresh',password:'1234'})
+        } else {
+            reject('Error: something went wrong')
+        }
+    },1000)
+})
+
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username
+})
+.then((username)=>{
+
+    console.log(username)
+
+})
+.catch((err)=>{
+    console.log(err)
+}).finally(()=>{
+    console.log("the promise is resolve or rejected")
+})
+
+const promiseFive = new Promise((resolve, reject)=>{
+
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({username:'Aumix',password:'1234'})
+        } else {
+            reject('Error: JS went wrong')
+        }
+    },1000)
+});
+
+//async and await are similar to then and catch
+
+// issue with async and await is they can't handle error directly we have to use try catch block
+async function consumePromiseFive (){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+
+
+    }
+}
+
+consumePromiseFive();
+
+
